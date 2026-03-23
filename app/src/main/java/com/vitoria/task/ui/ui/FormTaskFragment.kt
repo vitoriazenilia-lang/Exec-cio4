332f4d524a25f979e8d82ed107f5535ceb379cbb
vitoriazenilia-lang <vitoriazenilia@gmail.com>
2026-03-23 18:26:47 -0300

    Passo 96

--- app/src/main/java/com/vitoria/task/ui/ui/FormTaskFragment.kt
+++ app/src/main/java/com/vitoria/task/ui/ui/FormTaskFragment.kt
@@ -6,15 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.vitoria.task.ui.R
-class TodoFragment : Fragment() {
 
+
+class FormTaskFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_todo, container, false)
+        return inflater.inflate(R.layout.fragment_form_task, container, false)
     }
 
 
--- app/src/main/java/com/vitoria/task/ui/ui/HomeFragment.kt
+++ app/src/main/java/com/vitoria/task/ui/ui/HomeFragment.kt
@@ -5,17 +5,49 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TableLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.vitoria.task.ui.R
+import com.vitoria.task.ui.databinding.FragmentHomeBinding
+import com.vitoria.task.ui.ui.adapter.ViewPagerAdapter
 
 class HomeFragment : Fragment() {
 
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        iniTabs()
+    }
+
+
+    private fun iniTabs() {
+        val pageAdapter = ViewPagerAdapter(requireActivity())
+        binding.viewPager.adapter = pageAdapter
+        pageAdapter.addFragment(TodoFragment(), R.string.status_task_todo)
+        pageAdapter.addFragment(TodoFragment(), R.string.status_task_doing)
+        pageAdapter.addFragment(TodoFragment(), R.string.status_task_done)
+
+        binding.viewPager.offscreenPageLimit= pageAdapter.itemCount
+
+        TabLayoutMediator(binding.tabs, binding.viewPager) { tab, position ->
+            tab.text = getString(pageAdapter.getTitle(position))
+        }.attach()
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
